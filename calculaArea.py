f69284555c805f5298f23cfc2a3a5064719ec588
Álvaro Malheiros Lima Lombardi <lombardialvaro@outlook.com>
2025-09-18 21:45:13 -0300

    Delete calculaArea.py

--- calculaArea.py
+++
@@ -1,7 +0,0 @@
-class Calcula_Area:
-    def __init__(self,altura,comprimento):
-        self.altura = altura
-        self.comprimento = comprimento
-    def area(self):
-        area = self.altura * self.comprimento
-        return area
